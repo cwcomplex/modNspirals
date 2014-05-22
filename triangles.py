b8ed19a63a3dec2d99708a4f2f8685ed5c857e48
andrewreiter <arr@watson.org>
2014-05-22 09:58:21 -0400

    - Add commented out validator - Validator used to find bug I noticed while inspecting images - Now generates images to .png's of appropriate size, not oversized block - This complets the code for triangles that bounce back and forth from x/y axis - Need another code for sweeping in one direction

--- triangles.py
+++ triangles.py
@@ -11,26 +11,29 @@ import time
 
 def generateImages(block, maxSquareSize, N, fileprefix):
   
-  ks = block.keys()
-#  xm = max(ks, key=itemgetter(0))[0]
-#  ym = min(ks, key=itemgetter(1))[0]
-#  print block
   im = Image.new("L", (maxSquareSize, maxSquareSize))
   step = int(255/N) + 1
   for rc in block.keys():
-      im.putpixel((rc[0], rc[1]-1), (block[rc[0], rc[1]]) * step)
+#      im.putpixel((rc[0], rc[1]), 255)   # validator to make sure we miss no pixels
+      im.putpixel((rc[0], rc[1]), (block[rc[0], rc[1]]) * step)
   im.save(fileprefix+".png")
   del im
 
 
 def generateTriangle(N, k):
   modNset = range(0, N);
-# Need to think this through
-  maxBlock = k*k*N
-  maxIteration = k*k*k*N*N
+
+#
+# Need to think maxBlock, maxIteration through more
+# just making this large since I dont know the generation formula
+# then will cut the size down on image generation
+#
+  maxBlock = k*k*k*N*N
+  maxIteration = k*k*k*N*N*N
 
   currentPosition = [ 0, maxBlock ]
   block = {}
+  prev = None
   dx = 0
   dy = 0
 
@@ -52,11 +55,11 @@ def generateTriangle(N, k):
 # we are moving up-left
       if dx == -1 and dy == -1:
         if prev[0] == 0:
-          print "You have reached x-min with mn=%d"%(mn)
+#          print "You have reached x-min with mn=%d"%(mn)
           if mn == (N-1):
-              tricount += 1
-              if tricount == k:
-                return block
+            tricount += 1
+            if tricount == k:
+              return block
           currentPosition[1] += -1
           dx = dy = 1
         else:
@@ -64,25 +67,41 @@ def generateTriangle(N, k):
           currentPosition[1] += dy
       elif dx == 1 and dy == 1:
         if prev[1] == maxBlock:
-          print "You have reached y-max with mn=%d" % (mn)
+#          print "You have reached y-max with mn=%d" % (mn)
           if mn == (N-1):
-              tricount += 1
-              if tricount == k:
-                return block
-          currentPosition[1] += -1
+            tricount += 1
+            if tricount == k:
+              return block
+          #currentPosition[1] += -1
           currentPosition[0] += 1
           dx = dy = -1
         else:
           currentPosition[0] += dx
           currentPosition[1] += dy
+  return None
 
 def main():
 
-  for N in range(2, 15):
-    for k in range(2, 15):
-      print "%d-%d" % (N,k)
-      blok = generateTriangle(N, k) 
-      generateImages(blok, N*k*k, N, 'tri-%d-%d'%(N,k))
+  for N in range(2, 30):
+    for k in range(1, 50):
+
+      block = generateTriangle(N, k) 
+      if block == None:
+        print "Failed to generate Tri(N=%d, k=%d)" % (N,k)
+        continue
+
+
+# We allocated more room than needed, so let's chop it down for image creation
+# by just mapping translation to smaller block.
+      keyset = block.keys()
+      maxxy = map(max, zip(*keyset))
+      maxy = maxxy[1]
+      minxy = map(min, zip(*keyset))
+      miny = minxy[1]
+      translated_block = {}
+      for ks in keyset:
+        translated_block[ks[0], ks[1]-miny] = block[ks[0], ks[1]]
+      generateImages(translated_block, maxy-miny+1, N, 'Tri-N%d-k%d'%(N,k))
 
 
 if __name__ == '__main__':
