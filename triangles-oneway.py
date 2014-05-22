4249d90789d2c3eaedbeff281c2f0df1159f72c3
andrewreiter <arr@watson.org>
2014-05-22 10:07:49 -0400

    only hav dx = dy = -1 .. don't bounce back and forth like twoway.py code

---
+++ triangles-oneway.py
@@ -0,0 +1,110 @@
+#!/usr/bin/python
+
+
+# 
+# This differs from triangles-twoway.py in that this
+# will only ever go in dx = dy = -1 direction and the
+# twoway code will change from dx = dy = (+/-)1
+#
+# It is not clear to me as to which depicts Ond more
+# appropriately at this time.
+#
+# (c) 2014 Andrew Reiter <arr@watson.org>
+# 
+
+import sys
+from PIL import Image
+#import gmpy
+from math import sqrt
+import operator
+from operator import itemgetter
+from collections import defaultdict
+import time
+
+def generateImages(block, maxSquareSize, N, fileprefix):
+  
+  im = Image.new("L", (maxSquareSize, maxSquareSize))
+  step = int(255/N) + 1
+  for rc in block.keys():
+#      im.putpixel((rc[0], rc[1]), 255)   # validator to make sure we miss no pixels
+      im.putpixel((rc[0], rc[1]), (block[rc[0], rc[1]]) * step)
+  im.save(fileprefix+".png")
+  del im
+
+
+def generateTriangle(N, k):
+  modNset = range(0, N);
+
+#
+# Need to think maxBlock, maxIteration through more
+# just making this large since I dont know the generation formula
+# then will cut the size down on image generation
+#
+  maxBlock = k*k*k*N*N
+  maxIteration = k*k*k*N*N*N
+
+  currentPosition = [ 0, maxBlock ]
+  block = {}
+  prev = None
+  dx = 0
+  dy = 0
+  xmax = 0
+  tricount = 0
+  for jj in range(0, maxIteration+5):
+    for mn in modNset:
+
+
+      prev = currentPosition
+      block[prev[0], prev[1]] = mn
+
+      if prev[0] == 0 and prev[1] == maxBlock:  # At start
+        xmax = 1
+        currentPosition[0] += 1 
+        currentPosition[1] += 0 
+        dx = -1
+        dy = -1
+        continue
+
+# we are moving up-left
+      if dx == -1 and dy == -1:
+        if prev[0] == 0:
+#          print "You have reached x-min with mn=%d"%(mn)
+          if mn == (N-1):
+            tricount += 1
+            if tricount == k:
+              return block
+
+          xmax += 1 
+          currentPosition[0] = xmax 
+          currentPosition[1] = maxBlock 
+        else:
+          currentPosition[0] += dx
+          currentPosition[1] += dy
+  return None
+
+def main():
+
+  for N in range(2, 5):
+    for k in range(1, 20):
+
+      block = generateTriangle(N, k) 
+      if block == None:
+        print "Failed to generate Tri(N=%d, k=%d)" % (N,k)
+        continue
+
+
+# We allocated more room than needed, so let's chop it down for image creation
+# by just mapping translation to smaller block.
+      keyset = block.keys()
+      maxxy = map(max, zip(*keyset))
+      maxy = maxxy[1]
+      minxy = map(min, zip(*keyset))
+      miny = minxy[1]
+      translated_block = {}
+      for ks in keyset:
+        translated_block[ks[0], ks[1]-miny] = block[ks[0], ks[1]]
+      generateImages(translated_block, maxy-miny+1, N, 'Tri-one-N%d-k%d'%(N,k))
+
+
+if __name__ == '__main__':
+  main()
