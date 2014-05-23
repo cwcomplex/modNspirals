41e657be229d368959a49c5ed17fbc0e9ae48996
andrewreiter <arr@watson.org>
2014-05-23 11:31:39 -0400

    maxblock as argumnet

--- hexagons-oneway.py
+++ hexagons-oneway.py
@@ -14,11 +14,9 @@ def generateImages(block, maxSquareSize, N, fileprefix):
   ks = block.keys()
   im = Image.new("L", (maxSquareSize+1, maxSquareSize+1))
   step = int(255/N) + 1
-  print maxSquareSize
   for rc in block.keys():
-      print rc
-#      im.putpixel((rc[0], rc[1]), 255) # validator
-      im.putpixel((rc[0], rc[1]), (block[rc[0], rc[1]]) * step)
+      im.putpixel((rc[0], rc[1]), 255) # validator
+#      im.putpixel((rc[0], rc[1]), (block[rc[0], rc[1]]) * step)
   im.save(fileprefix+".png")
   del im
 
@@ -51,13 +49,12 @@ def generateHexagon(N, k, maxBlock, maxIteration):
       block[prev[0], prev[1]] = mn
 
 # We have reached
-      print prev
       if prev[0] == maxx+1 and prev[1] == maxBlock:
         maxx += 1
         if mn == (N-1):
           hexcount += 1
           if hexcount == k:
-            return block
+            return (block, jj+1)
         dir_ind = 0
         move_count_lim += 1
         move_count_min += 1
@@ -72,17 +69,19 @@ def generateHexagon(N, k, maxBlock, maxIteration):
         if move_count == move_count_lim:
           dir_ind += 1
           move_count = 0
-  return None 
+  return (None, 0)
 
 
 def main():
 
-  for N in range(30, 31):
+  for N in range(32, 34):
     for k in range(1, 10):
-      print "%d-%d" % (N,k)
       maxBlock = N*k
       maxIteration = maxBlock*N*N
-      block = generateHexagon(N, k, maxBlock, maxIteration) 
+      (block, iters) = generateHexagon(N, k, maxBlock, maxIteration) 
+      if block == None:
+        print "Unable to create Hex(%d, %d)" % (N, k)
+        continue
       keyset = block.keys()
       maxxy = map(max, zip(*keyset))
       maxy = maxxy[1]
@@ -91,7 +90,8 @@ def main():
       translated_block = {}
       for ks in keyset:
         translated_block[ks[0], ks[1]-miny] = block[ks[0], ks[1]]
-      generateImages(translated_block, maxy-miny, N, 'hex-N%d-k%d'%(N,k))    
+      print "%d %d maxx=%d iters=%d" % (N, k, maxxy[0], iters)
+      generateImages(translated_block, maxy-miny, N, 'hex-one-N%d-k%d'%(N,k))    
 #      generateImages(block, maxBlock, N, 'hex-%d-%d'%(N,k))
 
 
